4a6b785116a52336dd7d8d4b9540e1ab0eea321b
Wayra <wayrasoft10@gmail.com>
2025-03-31 20:40:07 +1100

    Part 1 - Q1.2
    Array numbers sorted in ascending order and printed in console.

--- arrayPart1.js
+++ arrayPart1.js
@@ -3,8 +3,16 @@
 // Author: Carlos
 // Date: 31/03/2025
 
-// Array definition
+// Q1.1 Array definition (array of unsorted numbers)
 const numbers = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
 
 // Print the array in console
+console.log(numbers);
+
+// Q1.2 Sort the array in ascending order
+// "a" represents the first number being compared in each pair
+// "b" represents the second number being compared in each pair
+numbers.sort(function(a, b) {return a - b});
+
+// Print the sorted array in console
 console.log(numbers);

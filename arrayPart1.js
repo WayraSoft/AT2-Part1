0630478af04706686678b4e558b594e19a409683
Wayra <wayrasoft10@gmail.com>
2025-04-01 12:04:42 +1100

    Part 1 - Q1.5
    Function sequentialSearch created with to two examples of the function behaviour (target found and not found)

--- arrayPart1.js
+++ arrayPart1.js
@@ -40,3 +40,39 @@ numbers.splice(2, 1);
 numbers.splice(10, 1);
 // Print the sorted array in console.
 console.log(numbers);
+
+
+// Q1.5 Implement the function "sequentialSearch" to find an element within
+// the array.
+// The function accepts Two parameter, the array and the value to be searched
+// and it will return a value of the index if the element is found, or -1 if the 
+// element is not found.
+function sequentialSearch(numbArray, targetNumb) {
+  let index = -1;      // local variable, init as 'not found' value.
+  for(let i = 0; i < numbArray.length; i++) {
+    // it loops through every element of the array and compares values.
+    if(numbArray[i] == targetNumb) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
+
+// Example when the target is found:
+// let indexTarget = sequentialSearch(numbers, 25);
+// if(indexTarget >= 0) {
+//   console.log(`The target was found in the position ${indexTarget}`);
+// }
+// else {
+//   console.log(`The target was not found: ${indexTarget}`);
+// }
+
+// Example when the target is not found:
+// let indexTarget = sequentialSearch(numbers, 15);
+// if(indexTarget >= 0) {
+//   console.log(`The target was found in the position ${indexTarget}`);
+// }
+// else {
+//   console.log(`The target was not found: ${indexTarget}`);
+// }
